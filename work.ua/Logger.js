479a118f5a8011c937e7a44d167b485550bdc6e8
Andrii Kladochnyi <aklad@softserveinc.com>
2019-08-27 18:11:39 +0300

    work.ua - get job titles and log to file

---
+++ work.ua/Logger.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const formatDate = require("date-fns/format")
+
+class Logger{
+    constructor(folder = "./"){
+        const now = new Date();
+
+        if(folder != "./"){
+            if(!fs.existsSync(folder)){
+                fs.mkdir(folder, error => {
+                    if(error){
+                        console.error(">>> Creating dir error: ", error);
+                    }
+                }); 
+            }
+        }
+        this.firstLog = true;
+        this.fileName = folder + "/" + formatDate(now, formatDate(now,"yyyy-MM-ddTHH-mm-ss")) + ".txt";
+    }
+
+    log(value) {
+        return new Promise((resolve, reject) => {
+            let val = this.firstLog ? value : `\r\n${value}`; 
+            this.firstLog = false;
+            fs.appendFile(`${this.fileName}`, val, 'utf8', error => {
+                if(error){
+                    console.error(">>> Logging error: ", error);
+                    reject(">>> Logging error: ", error);
+                    return;
+                }
+                resolve();
+            });
+        });
+    }
+}
+
+module.exports = Logger;
---
+++ work.ua/index.js
@@ -0,0 +1,51 @@
+const puppeteer = require('puppeteer');
+const Logger = require("./Logger");
+const logger = new Logger("logs");
+let url = "https://www.work.ua/en/jobs-it/?days=122"//"https://www.work.ua/en/jobs-it/?days=122";
+let jobList = [];
+let pageN = 1;
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    await page.goto(url);
+    
+    jobList = await processPage(page);
+
+    await browser.close();
+
+    logger.log(jobList.reduce((str, job) => `${str}${job}\n`, ""));
+})();
+
+
+async function processPage(page){
+    console.log(">>> Page ", pageN++);
+    let newJobs = await page.$$eval(
+                        '#pjax-job-list h2 a', 
+                        jobTitles => jobTitles.map(el => el.innerText)
+                    );
+
+    console.log(newJobs);
+
+    const nextPageUrl = await page.evaluate(() => {
+        let nextBtn = 
+            document.querySelector(".pagination:first-child li:last-child");
+        
+        if(nextBtn.classList.contains("disabled")){
+            return null;
+        } else {
+            return nextBtn.querySelector('a').href;
+        }
+    });
+
+    if(nextPageUrl){
+        await page.goto(nextPageUrl);
+        newJobs = [...newJobs, ...await processPage(page)];
+    } else {
+        console.log(">>> LAST PAGE!");
+    }
+    
+    
+    return newJobs;
+}
