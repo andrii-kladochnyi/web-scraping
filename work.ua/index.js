7196400856eb22d1ad47bbab75bb005fc61fdf28
Andrii Kladochnyi <kladochny.andriy@hotmail.com>
2019-08-28 11:24:21 +0300

    work.ua: sort json

--- work.ua/index.js
+++ work.ua/index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const Logger = require("./Logger");
 const logger = new Logger("logs", "json");
-let url = "https://www.work.ua/en/jobs-it/?days=122";//"https://www.work.ua/en/jobs-sumy-it/";
+let url = "https://www.work.ua/en/jobs-it/?days=122";//"https://www.work.ua/en/jobs-it/?days=122";//"https://www.work.ua/en/jobs-sumy-it/";
 let jobList = [];
 let pageN = 1;
 
@@ -16,17 +16,22 @@ let pageN = 1;
     await browser.close();
 
     //logger.log(jobList.reduce((str, job) => `${str}${job}\n`, ""));
+
     logger.log(
         JSON.stringify(
-            jobList.reduce((obj, job) => {
-                if(obj[job]){
-                    obj[job]++;
-                } else {
-                    obj[job] = 1;
-                }
-
-                return obj;
-            }, {})
+            Object.entries(
+                jobList.reduce((obj, job) => {
+                    const j = job.toLowerCase();
+                    if(obj[j]){
+                        obj[j]++;
+                    } else {
+                        obj[j] = 1;
+                    }
+                    return obj;
+                }, {})
+            )
+            .sort(([key1, val1], [key2, val2]) => val2-val1)
+            .reduce((obj, [key, val]) => {obj[key] = val; return obj}, {})
         )
     );
 })();
